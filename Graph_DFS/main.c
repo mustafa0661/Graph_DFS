bcfa9895442d22ff3c21219a5e3e51d00567ec1c
Mustafa <mustafa.uzun1961ts@gmail.com>
2023-05-19 21:35:28 +0300

    Okuma fonksiyonu eklendi

--- Graph_DFS/main.c
+++ Graph_DFS/main.c
@@ -6,9 +6,37 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int array[6][6];
+bool visited[6];
+
+void readMatrix(){
+    
+    int i = 0;
+    FILE *fp = fopen("matrix.txt", "r");
+    if(fp == 0) {
+        perror("fopen");
+        exit(1);
+    }
+    while(fscanf(fp, "%d %d %d %d %d %d",
+                 
+                 &array[i][0],
+                 &array[i][1],
+                 &array[i][2],
+                 &array[i][3],
+                 &array[i][4],
+                 &array[i][5]
+                 ) != EOF)
+    {
+        i = i + 1;
+    }
+    fclose(fp);
+}
+
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    readMatrix();
     return 0;
 }
